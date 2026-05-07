df8dd81c286322c069df9b8e1d7a4b2d189125b5
Nguyen Tuan Minh <aireakminh205@gmail.com>
2026-05-07 23:28:14 +0700

    Init project

---
+++ ticket_service/src/test/java/org/film/management/ticket/service/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.film.management.ticket.service;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
